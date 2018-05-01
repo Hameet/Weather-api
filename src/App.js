752f681c0b8d1ebbc44e3094caae1cba498ac929
Hameet <hameet.singh@hotmail.com>
2018-05-02 09:34:28 +1200

    modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Titles from './components/Titles'
+// import Titles from './components/Titles'
 import Form from './components/Form'
 import Weather from './components/Weather'
 import Animate from './components/Animate'
